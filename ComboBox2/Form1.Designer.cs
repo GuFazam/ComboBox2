f7d7cc4ad9525a73bccfe8d2a7ffb0b0e92c8895
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-03-08 21:59:52 -0300

    usamo if com a combolist mas falta concertar um erro

--- ComboBox2/Form1.Designer.cs
+++ ComboBox2/Form1.Designer.cs
@@ -31,13 +31,13 @@
             this.btnAdicionar = new System.Windows.Forms.Button();
             this.btnRemover = new System.Windows.Forms.Button();
             this.textBoxNome = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmblista = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnAdicionar
             // 
-            this.btnAdicionar.Location = new System.Drawing.Point(338, 177);
+            this.btnAdicionar.Location = new System.Drawing.Point(383, 177);
             this.btnAdicionar.Name = "btnAdicionar";
             this.btnAdicionar.Size = new System.Drawing.Size(83, 23);
             this.btnAdicionar.TabIndex = 0;
@@ -53,31 +53,32 @@
             this.btnRemover.TabIndex = 1;
             this.btnRemover.Text = "Remover";
             this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
             // 
             // textBoxNome
             // 
-            this.textBoxNome.Location = new System.Drawing.Point(264, 89);
+            this.textBoxNome.Location = new System.Drawing.Point(216, 89);
             this.textBoxNome.Name = "textBoxNome";
-            this.textBoxNome.Size = new System.Drawing.Size(157, 20);
+            this.textBoxNome.Size = new System.Drawing.Size(250, 20);
             this.textBoxNome.TabIndex = 2;
             // 
-            // comboBox1
+            // cmblista
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(216, 136);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(205, 21);
-            this.comboBox1.TabIndex = 3;
+            this.cmblista.FormattingEnabled = true;
+            this.cmblista.Location = new System.Drawing.Point(216, 136);
+            this.cmblista.Name = "cmblista";
+            this.cmblista.Size = new System.Drawing.Size(250, 21);
+            this.cmblista.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(213, 90);
+            this.label1.Location = new System.Drawing.Point(92, 90);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.Size = new System.Drawing.Size(118, 15);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Nome";
+            this.label1.Text = "Digite seu Nome:";
             // 
             // Form1
             // 
@@ -85,12 +86,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(660, 308);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cmblista);
             this.Controls.Add(this.textBoxNome);
             this.Controls.Add(this.btnRemover);
             this.Controls.Add(this.btnAdicionar);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,7 +103,7 @@
         private System.Windows.Forms.Button btnAdicionar;
         private System.Windows.Forms.Button btnRemover;
         private System.Windows.Forms.TextBox textBoxNome;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cmblista;
         private System.Windows.Forms.Label label1;
     }
 }
--- ComboBox2/Form1.cs
+++ ComboBox2/Form1.cs
@@ -20,9 +20,30 @@ namespace ComboBox2
         private void btnAdicionar_Click(object sender, EventArgs e)
         {
             if (textBoxNome.TextLength ==0)
-            {
                 MessageBox.Show("Por favor digite algo", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 textBoxNome.Focus();
+            {
+
+                {
+                    cmblista.Items.Add(textBoxNome.Text);
+                    textBoxNome.Clear();
+                    textBoxNome.Focus();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //cmblista.SelectedIndex = 0;
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (cmblista.SelectedIndex == 0)
+                MessageBox.Show("Por favor selecione um item", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            {
+                cmblista.Items.Remove(cmblista.SelectedItem);
+                cmblista.SelectedItem = 0;
             }
         }
     }
